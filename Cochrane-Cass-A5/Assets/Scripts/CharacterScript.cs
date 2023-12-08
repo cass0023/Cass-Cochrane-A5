7ecc0fa43b97b64c02d312aa3540bb50186a5e71
cass0023 <150483589+cass0023@users.noreply.github.com>
2023-12-08 18:57:21 -0500

    Add platform spacing and levels

--- Cochrane-Cass-A5/Assets/Scripts/CharacterScript.cs
+++ Cochrane-Cass-A5/Assets/Scripts/CharacterScript.cs
@@ -4,30 +4,28 @@ using UnityEngine;
 
 public class CharacterScript : MonoBehaviour
 {
-    public Rigidbody2D rb;
-    public float jumpSpeed;
-    public float speed = 10f;
+    public Rigidbody2D characterRb;
+    public float speed = 8f;
 
     private float moveX;
-    // Start is called before the first frame update
+
     void Start()
     {
-        rb.GetComponent<Rigidbody2D>();
+        characterRb.GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        moveX = Input.GetAxis("Horizontal") * speed;
-        if (Input.GetKeyDown(KeyCode.Space) == true)
-        {
-            rb.velocity = Vector2.up * jumpSpeed;
-        }
+        //creates x movement and adds speed every level passed
+        float newSpeed = speed;
+        moveX = Input.GetAxis("Horizontal") * newSpeed;
     }
     private void FixedUpdate()
     {
-        Vector2 velocity = rb.velocity;
+        Vector2 velocity = characterRb.velocity;
         velocity.x = moveX;
-        rb.velocity = velocity;
+        characterRb.velocity = velocity;
     }
 }
--- Cochrane-Cass-A5/Assets/Scripts/GameManager.cs
+++ Cochrane-Cass-A5/Assets/Scripts/GameManager.cs
@@ -5,23 +5,34 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public GameObject platformPrefab;
-    public float platformCount;
-    public Vector3 spawnPosition;
-    // Start is called before the first frame update
+    public float platformsPerLevel;
+    public GameObject characterLocation;
+    private Vector3 spawnPosition;
+    private float levelCheck;
+    private float numberOfUnits = 2;
+    private float lastPlatformY;
+
     void Start()
     {
-        Vector3 spawnPosition = new Vector3();
-        for (float i = 0; i < platformCount; i++)
-        {
-            spawnPosition.y = Random.Range(-15f, 100f);
-            spawnPosition.x = Random.Range(-12f, 12f);
-            Instantiate(platformPrefab, spawnPosition, Quaternion.identity);
-        }
+        levelCheck = -platformsPerLevel / 2;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        //creates platforms into levels, starting with 2 units per platform
+        if (characterLocation.transform.position.y > levelCheck)
+        {
+            spawnPosition = new Vector3();
+            for (float i = 0; i < platformsPerLevel; i++)
+            {
+                spawnPosition.y = i * numberOfUnits + lastPlatformY;
+                spawnPosition.x = Random.Range(-2.5f, 2.5f);
+                Instantiate(platformPrefab, spawnPosition, Quaternion.identity);
+            }
+            levelCheck += platformsPerLevel * numberOfUnits;
+            lastPlatformY += platformsPerLevel * numberOfUnits;
+            numberOfUnits++;
 
+        }
     }
 }
